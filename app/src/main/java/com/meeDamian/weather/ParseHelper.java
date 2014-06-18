8b4dea9bd5651c6cbccf5e1fb6efb6de89638255
Damian Mee <damian.wasilewski@gmail.com>
2014-06-18 03:25:05 +0200

    parse key moved where it belongs; build script improved

--- app/src/main/java/com/meeDamian/weather/ParseHelper.java
+++ app/src/main/java/com/meeDamian/weather/ParseHelper.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
-import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.GetDataCallback;
 import com.parse.Parse;
@@ -17,7 +16,7 @@ public class ParseHelper {
 	private static ParseHelper instance;
 
 	private ParseHelper(Context c ) {
-		Parse.initialize(c, "dMeWN03ARR2f0OuFaytujYpdbjnTqRXpw8k1DRdN", "vOlFqHHbrNaW4urMuPT9jOQKcZYVsTWLjl91tobF");
+		Parse.initialize(c, "dMeWN03ARR2f0OuFaytujYpdbjnTqRXpw8k1DRdN", BuildConfig.PARSE_KEY);
 	}
 
 	public static void getCurrentWeather(final String currentId, final OnCurrentWeather callback) {
@@ -27,48 +26,30 @@ public class ParseHelper {
 		query.getFirstInBackground(new GetCallback<ParseObject>() {
 			@Override
 			public void done(ParseObject weatherObject, ParseException e) {
-				if( e==null && (currentId==null || !weatherObject.getObjectId().equals(currentId)) ) {
+			if( e==null && (currentId==null || !weatherObject.getObjectId().equals(currentId)) ) {
 
-					final String myId = weatherObject.getObjectId();
-					String tmpTitle = weatherObject.getString("title");
-					String tmpDesc = weatherObject.getString("description");
+				final String myId = weatherObject.getObjectId();
+				String tmpTitle = weatherObject.getString("title");
+				String tmpDesc = weatherObject.getString("description");
 
-					ParseObject baseWeather = weatherObject.getParseObject("base");
-					if( baseWeather!=null ) {
+				ParseObject baseWeather = weatherObject.getParseObject("base");
+				if( baseWeather!=null ) {
 
-						final String baseId = baseWeather.getObjectId();
-						final String title = ( tmpTitle==null ) ? baseWeather.getString("defaultTitle") : tmpTitle;
-						final String desc = ( tmpDesc==null ) ? baseWeather.getString("defaultDescription") : tmpDesc;
+					final String baseId = baseWeather.getObjectId();
+					final String title = ( tmpTitle==null ) ? baseWeather.getString("defaultTitle") : tmpTitle;
+					final String desc = ( tmpDesc==null ) ? baseWeather.getString("defaultDescription") : tmpDesc;
 
-						ParseHelper.setImage(baseWeather, new GetDataCallback() {
-							@Override
-							public void done(byte[] bytes, ParseException e) {
-							Bitmap image = ( e==null ) ? BitmapFactory.decodeByteArray(bytes, 0, bytes.length) : null;
-							callback.onDataAvailable(myId, baseId, title, desc, image);
-							}
-						});
-					}
+					baseWeather.getParseFile("image").getDataInBackground(new GetDataCallback() {
+						@Override
+						public void done(byte[] bytes, ParseException e) {
+						Bitmap image = (e==null) ? BitmapFactory.decodeByteArray(bytes, 0, bytes.length) : null;
+						callback.onDataAvailable(myId, baseId, title, desc, image);
+						}
+					});
 				}
 			}
+			}
 		});
-	}
-
-
-
-	public static void getWeatherById(String id, GetCallback<ParseObject> callback) {
-		ParseQuery.getQuery("Weather").getInBackground(id, callback);
-	}
-
-	public static ParseQuery<ParseObject> getAllWeathers(FindCallback<ParseObject> callback) {
-		ParseQuery<ParseObject> weatherQuery = ParseQuery.getQuery("Weather");
-		weatherQuery.orderByDescending("timesUsed");
-		weatherQuery.findInBackground(callback);
-
-		return weatherQuery;
-	}
-
-	public static void setImage(ParseObject currentWeather, GetDataCallback callback) {
-		currentWeather.getParseFile("image").getDataInBackground(callback);
 	}
 
 	public static ParseHelper getInstance(Context c) {
